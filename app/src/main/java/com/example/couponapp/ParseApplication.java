3f9d0a04163010b6f095aa7b158bede44b0de7a0
azwadd <azwad_shameem@outlook.com>
2021-04-23 16:31:11 -0400

    Edited loginActivity.xml and made API's hidden - Azwad

--- app/src/main/java/com/example/couponapp/ParseApplication.java
+++ app/src/main/java/com/example/couponapp/ParseApplication.java
@@ -7,15 +7,11 @@ import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
-    // Initializes Parse SDK as soon as app is created
     @Override
     public void onCreate() {
         super.onCreate();
-
-        //Register your parse models
-        ParseObject.registerSubclass(Post.class);
-
-        // Register your parse models
+        // Initialize Parse SDK as soon as app is created
+        ParseObject.registerSubclass(Post.class);  // Register your parse models
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(BuildConfig.applicationID)
                 .clientKey(BuildConfig.clientKey)
