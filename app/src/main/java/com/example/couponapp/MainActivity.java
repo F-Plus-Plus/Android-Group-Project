68a39bde8588de62e9fab355c185474858d05041
Dewan <dtahmid000@citymail.cuny.edu>
2021-04-30 11:24:18 -0400

    Navigation Bar Buttons

--- app/src/main/java/com/example/couponapp/MainActivity.java
+++ app/src/main/java/com/example/couponapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.couponapp.fragments.ComposeFragment;
+import com.example.couponapp.fragments.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,17 +29,20 @@ public class MainActivity extends AppCompatActivity {
             Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.action_home:
+                    fragment = new HomeFragment();
                     Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.action_compose:
+                    fragment = new ComposeFragment();
                     Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.action_profile:
                 default:
+                    fragment = new ComposeFragment();
                     Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
                     break;
             }
-            // fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+            fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
             return true;
         });
         bottomNavigation.setSelectedItemId(R.id.action_home);
